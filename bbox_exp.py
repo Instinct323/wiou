d66388c77ff60584e1a8423b3d3b79370333be46
TongZJ <1400721986@qq.com>
2023-02-22 21:36:31 +0800

    Add files via upload

---
+++ bbox_exp.py
@@ -0,0 +1,225 @@
+import math
+import time
+
+import matplotlib.patches as pch
+import matplotlib.pyplot as plt
+import torch
+
+from iou import IoU_Cal
+from optimize import minimize
+
+red = 'orangered'
+orange = 'darkorange'
+yellow = 'gold'
+green = 'greenyellow'
+cyan = 'aqua'
+blue = 'deepskyblue'
+purple = 'mediumpurple'
+pink = 'violet'
+
+plt.rcParams['figure.dpi'] = 150
+
+COLORS = [purple, blue, green, yellow, orange]
+
+
+def run_once(func):
+    def handler(*args, **kwargs):
+        while True:
+            try:
+                return func(*args, **kwargs)
+            except Exception as reason:
+                print(reason)
+                time.sleep(20)
+                continue
+
+    return handler
+
+
+def xywh_to_ltrb(attr):
+    attr[..., :2] -= attr[..., 2: 4] / 2
+    attr[..., 2: 4] += attr[..., :2]
+    return attr
+
+
+def scatter_circle(n, radius, dot=[0., 0.], alpha=3):
+    ''' Generate scatter uniformly in a circular area'''
+    rho = torch.log(torch.rand(n) * (math.exp(alpha) - 1) + 1) / alpha * radius
+    theta = torch.rand(n) * math.tau
+    x = torch.cos(theta) * rho + dot[0]
+    y = torch.sin(theta) * rho + dot[1]
+    return x, y
+
+
+@run_once
+def simulate_exp(loss_fcn, lr=.01, max_iter=120,
+                 plot_points=False,
+                 n_points=None,
+                 major_cases=False,
+                 target_boxes_area=1 / 32,
+                 anchor_boxes_areas=[1 / 32, 1 / 24, 3 / 64, 1 / 16, 1 / 12, 3 / 32, 1 / 8],
+                 aspect_ratios=[1 / 4, 1 / 3, 1 / 2, 1, 2, 3, 4]):
+    ''' loss_fcn: BBR losses used in simulation experiment
+        plot_points: Display the anchor point distribution map
+        n_points: The number of randomly generated anchors
+        major_cases: Only the main cases in the regression process are addressed
+        target_boxes_areas: The area of the target box
+        anchor_boxes_areas: Area of anchor boxes
+        aspect_ratios: Aspect ratio of bounding boxes'''
+    IoU = IoU_Cal.IoU
+    IoU_Cal.iou_mean = 1.
+    aspect_ratios = torch.tensor(aspect_ratios)
+    anchor_boxes_areas = torch.tensor(anchor_boxes_areas)
+    # The distribution pattern of the regression cases
+    points_radius = 0.1 if major_cases else 0.5
+    max_iter = max_iter // 2 if major_cases else max_iter
+    n_points = n_points if n_points else int(2e4 * points_radius ** 2)
+    # The coordinates need to be transformed to [0, 1]
+    x, y = scatter_circle(n_points, radius=points_radius, dot=[.5, .5])
+    # 7*7 anchor boxes are generated at each anchor point
+    width = (anchor_boxes_areas[:, None] / aspect_ratios).sqrt()
+    height = aspect_ratios * width
+    width, height = map(torch.flatten, [width, height])
+    # Splice and get all anchor boxes
+    xy = torch.stack([x, y], dim=-1)
+    wh = torch.stack([width, height], dim=-1)
+    anchor = torch.cat([xy[:, None].repeat(1, len(width), 1),
+                        wh[None].repeat(len(x), 1, 1)], dim=-1)[..., None, :]
+    # Get the target box
+    target_w = (target_boxes_area / aspect_ratios).sqrt()
+    target_h = target_w * aspect_ratios
+    target = torch.cat([torch.full([len(aspect_ratios), 2], 0.5),
+                        target_w[:, None], target_h[:, None]], dim=-1)
+    anchor, target = map(xywh_to_ltrb, [anchor, target])
+    anchor = anchor.repeat(1, 1, len(aspect_ratios), 1)
+    # Draw the anchor point distribution map
+    if plot_points:
+        fig = plt.subplot()
+        plt.scatter(x, y, s=0.3, color=blue)
+        for axis in 'xy': getattr(plt, f'{axis}lim')([-0.05, 1.05])
+        for l, t, r, b in target:
+            rect = pch.Rectangle((l, t), (r - l), (b - t), alpha=0.2, facecolor=purple)
+            fig.add_patch(rect)
+        plt.show()
+    # Construct the loss function and solve it using the function <minimize>
+    result, _, log = minimize(anchor.detach(), lambda x: loss_fcn(x, target).mean(), lr=lr,
+                              eval_fcn=lambda x: IoU(x.detach(), target).mean(),
+                              max_iter=max_iter, prefix=loss_fcn.__name__)
+    loss = IoU(result, target).mean(dim=(1, 2))
+    loss_fcn = loss_fcn.__name__
+    print(f'{loss_fcn}: Mean IoU = {1 - loss.mean():.3f}, Min IoU = {1 - loss.max():.3f}')
+    # Draw the heat map of the IoU loss
+    # fig = plt.subplot(projection='3d')
+    # plt.title(loss_fcn)
+    # fig.set_xlabel('x')
+    # fig.set_ylabel('y')
+    # fig.set_zlabel('IoU')
+    # fig.view_init(40, 30)
+    # fig.scatter(x, y, loss, cmap=plt.get_cmap('rainbow'), c=(loss - loss.min()) / (loss.max() - loss.min()))
+    return {loss_fcn: log}
+
+
+def plot_loss(fcn_list, **simlate_kwargs):
+    ''' Draw the IoU loss curve
+        fcn_list: List of loss functions participating in the test
+        simlate_kwargs: The keyword argument of function <simulate_exp>'''
+    assert len(COLORS) >= len(fcn_list), 'Insufficient amount of color provided'
+    log_dict = {}
+    for fcn in fcn_list:
+        log_dict.update(simulate_exp(fcn, **simlate_kwargs))
+    fig = plt.subplot()
+    for key in 'right', 'top':
+        fig.spines[key].set_color('None')
+    plt.xlabel('Epochs')
+    plt.ylabel('IoU')
+    for color, fcn in zip(COLORS, log_dict):
+        log = log_dict[fcn]
+        x = torch.arange(1, len(log) + 1)
+        plt.plot(x, log, label=fcn, color=color)
+    plt.legend(frameon=False)
+    plt.show()
+
+
+def visualize_track(fcn_and_epoch: dict, lr=.01, colors=COLORS):
+    ''' Visual bounding box regression
+        fcn_and_epoch: {fcn: epoch ...}'''
+    assert len(colors) >= len(fcn_and_epoch), 'Insufficient amount of color provided'
+    IoU = IoU_Cal.IoU
+    anchor = xywh_to_ltrb(torch.tensor([[.7, .7, .2, .4],
+                                        [.5, .8, .6, .1]]))
+    target = xywh_to_ltrb(torch.tensor([[.2, .2, .05, .1],
+                                        [.5, .1, .05, .05]]))
+    # Fixed the format of key-value pairs
+    for fcn in fcn_and_epoch:
+        epoch = fcn_and_epoch[fcn]
+        if isinstance(epoch, int): fcn_and_epoch[fcn] = [epoch] * 2
+        assert len(fcn_and_epoch[fcn]) == 2
+    # The BBR is simulated using a gradient descent algorithm
+    for i in range(2):
+        fig = plt.subplot(1, 2, i + 1)
+        for f in [plt.xlim, plt.ylim]: f([0, 1])
+        for f in [plt.xticks, plt.yticks]: f([])
+        # for loc in ['top', 'bottom', 'left', 'right']: fig.spines[loc].set_color('None')
+        # Draw anchor boxes and target boxes
+        anc = pch.Rectangle(anchor[i][:2], *(anchor[i][2:] - anchor[i][:2]),
+                            edgecolor=orange, fill=False, label='Inital')
+        anc.set_zorder(1)
+        tar = pch.Rectangle(target[i][:2], *(target[i][2:] - target[i][:2]),
+                            edgecolor=red, fill=False, label='GT')
+        tar.set_zorder(1)
+        for p in [anc, tar]: fig.add_patch(p)
+        # Draws the anchor box in the optimization
+        for j, (color, fcn) in enumerate(zip(colors, fcn_and_epoch)):
+            epoch = fcn_and_epoch[fcn][i]
+            result = minimize(anchor[i].clone(), lambda x: fcn(x, target[i]), lr=lr,
+                              eval_fcn=lambda x: IoU(x.detach(), target[i]),
+                              max_iter=epoch, patience=None,
+                              prefix=fcn.__name__, title=not any([i, j]))[0]
+            res = pch.Rectangle(result[:2], *(result[2:] - result[:2]),
+                                facecolor=color, alpha=0.5, label=f'{fcn.__name__} {epoch} epochs')
+            res.set_zorder(-j)
+            fig.add_patch(res)
+        plt.legend(frameon=False)
+        plt.tight_layout()
+    plt.show()
+
+
+def plot_gain(gamma=[2.5, 1.9, 1.6, 1.4], delta=[2, 3, 4, 5],
+              colors=[pink, blue, yellow, orange]):
+    fig = plt.subplot()
+    for key in 'right', 'top':
+        fig.spines[key].set_color('None')
+    for key in 'left', 'bottom':
+        fig.spines[key].set_position(('data', 0))
+    # The outlier degree of bounding box
+    beta = torch.linspace(0, 8, 100)
+    for g, d, c in zip(gamma, delta, colors):
+        alpha = d * torch.pow(g, beta - d)
+        plt.plot(beta, beta / alpha, color=c, label=f'α={g}  δ={d}')
+    plt.plot(beta, torch.ones_like(beta), color='gray', linestyle='--', alpha=0.7)
+    # Sets the format of the axes
+    plt.xlabel('outlier degree')
+    plt.xticks(*[list(range(0, 9, 2)) * 2])
+    plt.ylabel('gradient gain')
+    plt.yticks(*[[0.5, 1, 1.5] * 2])
+    plt.ylim([0, 1.8])
+    plt.legend(frameon=False)
+    plt.show()
+
+
+if __name__ == '__main__':
+    f = IoU_Cal
+    f.monotonous = None
+    f.momentum = 0.06
+
+    # 0: Plot the bounding box regression loss in the simulation experiment
+    # 1: Visualize regression cases of simulation experiment
+    # 2: Visualize the trajectory of regression cases under the effect of WIoU loss and SIoU loss
+    # 3: Plot the relationship between the gradient multiplier r and the outlier degree β
+    plt.rcParams['figure.figsize'] = [4.0, 3.0]
+    command = [lambda: plot_loss([f.WIoU, f.SIoU, f.CIoU, f.DIoU, f.GIoU], n_points=500, major_cases=False),
+               lambda: simulate_exp(f.WIoU, plot_points=True, major_cases=True),
+               lambda: visualize_track({f.WIoU: 100, f.SIoU: 120, f.CIoU: 250, f.DIoU: 250},
+                                       colors=[purple, blue, green, pink]),
+               lambda: plot_gain()]
+
+    command[2]()
---
+++ optimize.py
@@ -0,0 +1,115 @@
+import logging
+from typing import Callable, Optional
+
+import torch
+from tqdm import tqdm
+
+logging.basicConfig(format='%(message)s', level=logging.INFO)
+LOGGER = logging.getLogger(__name__)
+
+torch.autograd.set_detect_anomaly(True)
+
+
+class minimize:
+    ''' x: The variable being optimized (torch.tensor)
+        loss_fcn: Loss function with x as input
+        lr: Learning rate
+        patience: Tolerance for no progress in optimization
+        eval_fcn: The loss function used for the evaluation
+        max_iter: Maximum number of iterations
+        prefix: Prefix of the progress bar
+        title: Show the title
+        return: optimal solution, min loss, log of loss'''
+
+    def __new__(cls,
+                x: torch.tensor,
+                loss_fcn: Callable,
+                lr: float,
+                eval_fcn=None,
+                patience: Optional[int] = 50,
+                max_iter: Optional[int] = None,
+                prefix: str = 'Minimize',
+                title: bool = True,
+                leave: bool = True):
+        assert patience or max_iter
+        # Initialize the variable
+        x.requires_grad = True
+        cls.x = x
+        cls.optimizer = torch.optim.Adam([x], lr=lr)
+        # Record the optimal solution
+        cls.min_loss, cls.best_variant, cls.log = float('inf'), None, []
+        if title: LOGGER.info(('%10s' * 3) % ('', 'cur_loss', 'min_loss'))
+        # Set class properties
+        cls.prefix = prefix
+        cls.leave = leave
+        instance = object.__new__(cls)
+        instance.loss_fcn, instance.eval_fcn = loss_fcn, eval_fcn
+        instance.main(patience, max_iter)
+        return instance.best_variant, instance.min_loss, instance.log
+
+    def main(self, patience, max_iter):
+        # Initialize the iteration-dependent parameters
+        pbar = tqdm(range(max_iter if max_iter else patience), leave=self.leave)
+        angry = 0 if patience else None
+        if not max_iter:
+            # Greedy method
+            while angry != patience:
+                is_better = self.update(pbar)
+                angry = 0 if is_better else angry + 1
+                pbar.reset() if is_better else pbar.update()
+        else:
+            for _ in pbar:
+                is_better = self.update(pbar)
+                # Lazy method
+                if patience:
+                    angry = 0 if is_better else angry + 1
+                    if angry == patience: break
+        pbar.close()
+
+    def update(self, pbar):
+        is_better = False
+        # Calculate the loss value and log it
+        loss = self.loss_fcn(self.x)
+        loss_value = loss.item() if not self.eval_fcn else self.eval_fcn(self.x).item()
+        self.log.append(loss_value)
+        # Save the optimal solution
+        if loss_value < self.min_loss:
+            self.min_loss, self.best_variant = loss_value, self.x.clone().detach()
+            is_better = True
+        # Back propagate the gradient and update the variable
+        loss.backward()
+        self.optimizer.step()
+        self.optimizer.zero_grad()
+        pbar.set_description(('%10s' + '%10.4g' * 2) % (self.prefix, loss_value, self.min_loss))
+        return is_better
+
+
+if __name__ == '__main__':
+    import matplotlib.pyplot as plt
+
+    x = torch.linspace(-3, 3, 50)
+    # x + 0.3 x^2 - 0.5 x^3 + 4 sin(x) + noise
+    y = x + 0.3 * x ** 2 - 0.5 * x ** 3 + 4 * torch.sin(x) + 5 * (torch.rand(len(x)) - 0.5)
+    plt.scatter(x, y, c='deepskyblue', label='true')
+
+
+    def cal_y(variant, x):
+        # x: [bs, 1] -> [bs, 4]
+        # a x + b x^2 + c x^3 + d e^x
+        x = torch.stack([x, x ** 2, x ** 3, torch.exp(-x)], dim=1)
+        # y: [bs, 4] × [4, ] -> [bs, ]
+        y = x @ variant
+        return y
+
+
+    def loss(variant):
+        pred_y = cal_y(variant, x)
+        return ((y - pred_y) ** 2).sum()
+
+
+    best_var, min_loss, log = minimize(torch.ones(4), loss_fcn=loss, lr=1e-1, patience=50, max_iter=2000)
+    print(best_var)
+    plt.plot(x, cal_y(best_var, x), c='orange', label='pred')
+
+    plt.legend()
+    plt.show()
