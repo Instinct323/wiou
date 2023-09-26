f539d7e94603a57e3d45d672b0ae01141199cdef
tongzj <1400721986@qq.com>
2023-09-26 21:10:11 +0800

    update

--- iou.py
+++ iou.py
@@ -4,7 +4,7 @@ import torch
 from torch import nn
 
 
-class IouLoss:
+class IouLoss(nn.Module):
     ''' n: Number of batches per training epoch
         t: The epoch when mAP's ascension slowed significantly
         monotonous: {
@@ -16,6 +16,7 @@ class IouLoss:
     delta = 3
 
     def __init__(self, n, t, ltype='WIoU', monotonous=False):
+        super().__init__()
         # The momentum of running mean
         time_to_real = n * t
         self.momentum = 1 - pow(0.05, 1 / time_to_real)
@@ -24,60 +25,52 @@ class IouLoss:
         self.monotonous = monotonous
 
         self.iou_mean = nn.Parameter(torch.tensor(1.), requires_grad=False)
-        self._is_train = isinstance(self.monotonous, bool)
 
-    def __setitem__(self, key, value):
-        self._fget[key] = value
-
-    def __getattr__(self, item):
+    def __getitem__(self, item):
         if callable(self._fget[item]):
             self._fget[item] = self._fget[item]()
         return self._fget[item]
 
     def __call__(self, pred, target, ret_iou=False, **kwargs):
-        # pred, target: x0,y0,x1,y1
-        self.pred, self.target = pred, target
         self._fget = {
+            # pred, target: x0,y0,x1,y1
+            'pred': pred,
+            'target': target,
             # x,y,w,h
-            'pred_xy': lambda: (self.pred[..., :2] + self.pred[..., 2: 4]) / 2,
-            'pred_wh': lambda: self.pred[..., 2: 4] - self.pred[..., :2],
-            'target_xy': lambda: (self.target[..., :2] + self.target[..., 2: 4]) / 2,
-            'target_wh': lambda: self.target[..., 2: 4] - self.target[..., :2],
+            'pred_xy': lambda: (self['pred'][..., :2] + self['pred'][..., 2: 4]) / 2,
+            'pred_wh': lambda: self['pred'][..., 2: 4] - self['pred'][..., :2],
+            'target_xy': lambda: (self['target'][..., :2] + self['target'][..., 2: 4]) / 2,
+            'target_wh': lambda: self['target'][..., 2: 4] - self['target'][..., :2],
             # x0,y0,x1,y1
-            'min_coord': lambda: torch.minimum(self.pred[..., :4], self.target[..., :4]),
-            'max_coord': lambda: torch.maximum(self.pred[..., :4], self.target[..., :4]),
+            'min_coord': lambda: torch.minimum(self['pred'][..., :4], self['target'][..., :4]),
+            'max_coord': lambda: torch.maximum(self['pred'][..., :4], self['target'][..., :4]),
             # The overlapping region
-            'wh_inter': lambda: torch.relu(self.min_coord[..., 2: 4] - self.max_coord[..., :2]),
-            's_inter': lambda: torch.prod(self.wh_inter, dim=-1),
+            'wh_inter': lambda: torch.relu(self['min_coord'][..., 2: 4] - self['max_coord'][..., :2]),
+            's_inter': lambda: torch.prod(self['wh_inter'], dim=-1),
             # The area covered
-            's_union': lambda: torch.prod(self.pred_wh, dim=-1) +
-                               torch.prod(self.target_wh, dim=-1) - self.s_inter,
+            's_union': lambda: torch.prod(self['pred_wh'], dim=-1) +
+                               torch.prod(self['target_wh'], dim=-1) - self['s_inter'],
             # The smallest enclosing box
-            'wh_box': lambda: self.max_coord[..., 2: 4] - self.min_coord[..., :2],
-            's_box': lambda: torch.prod(self.wh_box, dim=-1),
-            'l2_box': lambda: torch.square(self.wh_box).sum(dim=-1),
+            'wh_box': lambda: self['max_coord'][..., 2: 4] - self['min_coord'][..., :2],
+            's_box': lambda: torch.prod(self['wh_box'], dim=-1),
+            'l2_box': lambda: torch.square(self['wh_box']).sum(dim=-1),
             # The central points' connection of the bounding boxes
-            'd_center': lambda: self.pred_xy - self.target_xy,
-            'l2_center': lambda: torch.square(self.d_center).sum(dim=-1),
+            'd_center': lambda: self['pred_xy'] - self['target_xy'],
+            'l2_center': lambda: torch.square(self['d_center']).sum(dim=-1),
             # IoU
-            'iou': lambda: 1 - self.s_inter / self.s_union
+            'iou': lambda: 1 - self['s_inter'] / self['s_union']
         }
-        if self._is_train:
+
+        if self.training:
             self.iou_mean.mul_(1 - self.momentum)
-            self.iou_mean.add_(self.momentum * self.iou.detach().mean().item())
+            self.iou_mean.add_(self.momentum * self['iou'].detach().mean().item())
 
         loss = self._scaled_loss(getattr(self, f'_{self.ltype}')(**kwargs))
-        return (loss, self.iou) if ret_iou else loss
-
-    def train(self):
-        self._is_train = True
-
-    def eval(self):
-        self._is_train = False
+        return (loss, self['iou']) if ret_iou else loss
 
     def _scaled_loss(self, loss):
         if isinstance(self.monotonous, bool):
-            beta = self.iou.detach() / self.iou_mean
+            beta = self['iou'].detach() / self.iou_mean
             if self.monotonous:
                 loss *= beta.sqrt()
             else:
@@ -86,44 +79,44 @@ class IouLoss:
         return loss
 
     def _IoU(self):
-        return self.iou
+        return self['iou']
 
     def _WIoU(self):
-        dist = torch.exp(self.l2_center / self.l2_box.detach())
-        return dist * self.iou
+        dist = torch.exp(self['l2_center'] / self['l2_box'].detach())
+        return dist * self['iou']
 
     def _EIoU(self):
-        penalty = self.l2_center / self.l2_box \
-                  + torch.square(self.d_center / self.wh_box).sum(dim=-1)
-        return self.iou + penalty
+        penalty = self['l2_center'] / self['l2_box'] \
+                  + torch.square(self['d_center'] / self['wh_box']).sum(dim=-1)
+        return self['iou'] + penalty
 
     def _GIoU(self):
-        return self.iou + (self.s_box - self.s_union) / self.s_box
+        return self['iou'] + (self['s_box'] - self['s_union']) / self['s_box']
 
     def _DIoU(self):
-        return self.iou + self.l2_center / self.l2_box
+        return self['iou'] + self['l2_center'] / self['l2_box']
 
     def _CIoU(self, eps=1e-4):
         v = 4 / math.pi ** 2 * \
-            (torch.atan(self.pred_wh[..., 0] / (self.pred_wh[..., 1] + eps)) -
-             torch.atan(self.target_wh[..., 0] / (self.target_wh[..., 1] + eps))) ** 2
-        alpha = v / (self.iou + v)
-        return self.iou + self.l2_center / self.l2_box + alpha.detach() * v
+            (torch.atan(self['pred_wh'][..., 0] / (self['pred_wh'][..., 1] + eps)) -
+             torch.atan(self['target_wh'][..., 0] / (self['target_wh'][..., 1] + eps))) ** 2
+        alpha = v / (self['iou'] + v)
+        return self['iou'] + self['l2_center'] / self['l2_box'] + alpha.detach() * v
 
     def _SIoU(self, theta=4):
         # Angle Cost
-        angle = torch.arcsin(torch.abs(self.d_center).min(dim=-1)[0] / (self.l2_center.sqrt() + 1e-4))
+        angle = torch.arcsin(torch.abs(self['d_center']).min(dim=-1)[0] / (self['l2_center'].sqrt() + 1e-4))
         angle = torch.sin(2 * angle) - 2
         # Dist Cost
-        dist = angle[..., None] * torch.square(self.d_center / self.wh_box)
+        dist = angle[..., None] * torch.square(self['d_center'] / self['wh_box'])
         dist = 2 - torch.exp(dist[..., 0]) - torch.exp(dist[..., 1])
         # Shape Cost
-        d_shape = torch.abs(self.pred_wh - self.target_wh)
-        big_shape = torch.maximum(self.pred_wh, self.target_wh)
+        d_shape = torch.abs(self['pred_wh'] - self['target_wh'])
+        big_shape = torch.maximum(self['pred_wh'], self['target_wh'])
         w_shape = 1 - torch.exp(- d_shape[..., 0] / big_shape[..., 0])
         h_shape = 1 - torch.exp(- d_shape[..., 1] / big_shape[..., 1])
         shape = w_shape ** theta + h_shape ** theta
-        return self.iou + (dist + shape) / 2
+        return self['iou'] + (dist + shape) / 2
 
     def __repr__(self):
         return f'{self.__name__}(iou_mean={self.iou_mean.item():.3f})'
@@ -144,7 +137,7 @@ if __name__ == '__main__':
     iouloss.momentum = 1e-2
     print(iouloss)
 
-    for i in range(10000):
+    for i in range(5):
         pred, tar = xywh2xyxy(torch.rand([2, 3, 1, 4], requires_grad=True))
-        iouloss(pred, tar)
+        print(iouloss(pred, tar))
     print(iouloss)
