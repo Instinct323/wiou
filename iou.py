9396f4872f252ba163baf638572d8a2ca358b048
TongZJ <1400721986@qq.com>
2023-12-24 00:12:38 +0800

    update

--- iou.py
+++ iou.py
@@ -11,8 +11,8 @@ class IouLoss(nn.Module):
             False: non-monotonic FM
         }'''
     momentum = 1e-2
-    alpha = 1.5
-    delta = 3.4
+    alpha = 1.7
+    delta = 2.7
 
     def __init__(self, ltype='WIoU', monotonous=False):
         super().__init__()
